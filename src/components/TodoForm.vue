83070b1c7d2161f44f077398acd5fe3c617b871e
中村圭輝 <tel07360102@gmail.com>
2020-06-20 15:39:22 +0900

    add app files

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container>
+    <b-input-group>
+      <b-form-input type="text" v-model="value" />
+      <b-input-group-append>
+        <b-button variant="info">送信</b-button>
+      </b-input-group-append>
+    </b-input-group>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  data() {
+    return {
+      value: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Todoリスト</h1>
+    <todo-form />
+  </div>
+</template>
+
+<script>
+import TodoForm from '@/components/TodoForm'
+  ;
+
+export default {
+  name: 'Todo',
+  components: {
+    TodoForm,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
